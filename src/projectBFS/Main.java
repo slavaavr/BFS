2d0a6ed22035ddf3c4e4d5d267493d6486149ff0
slavaavr <_AVA@outlook.com>
2017-05-21 17:35:28 +0300

    v2

--- src/projectBFS/Main.java
+++ src/projectBFS/Main.java
@@ -1,5 +1,8 @@
 package projectBFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by slava on 10.11.2016.
  */
@@ -7,14 +10,17 @@ package projectBFS;
 public class Main {
     public static void main(String[] args)  {
         int [][]matrix = new int[][]{
-                {0, 1, 1, 0,0},
-                {1, 0, 0, 1,0},
-                {1, 0, 0, 0,1},
-                {0, 1, 0, 0,1},
-                {0, 0, 1, 1,0},
-                {0, 0, 1, 1,0}
+                {0,0,1,10},
+                {1,0,0,1},
+                {1,2,0,0},
+                {0,3,0,0}
         };
-        System.out.println(matrix.length);
+        NewBfs bfs = new NewBfs();
+        bfs.setMatrix(matrix);
+        System.out.println(bfs.run(1,2, TypeOutput.MINROUTE));
+        Queue<Integer> queue = new LinkedList<>();
+
+
     }
 
 }
